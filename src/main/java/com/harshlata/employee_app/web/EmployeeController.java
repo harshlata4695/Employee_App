dbe92a4f919f88a7564ff4be8f0e1fecb7ddd005
harshlata4695 <harshlatagupta48@gmail.com>
2023-05-28 12:52:33 +0530

    test

--- src/main/java/com/harshlata/employee_app/web/EmployeeController.java
+++ src/main/java/com/harshlata/employee_app/web/EmployeeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.harshlata.data.models.Employee;
 import com.harshlata.data.payload.request.EmployeeRequest;
@@ -59,5 +60,11 @@ public class EmployeeController {
 	{
 		employeeService.deleteEmployee(id);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@GetMapping(value= "/test")
+	public void test(@RequestParam("test") Boolean test)
+	{
+		System.out.println("tests....");
 	}
 }
